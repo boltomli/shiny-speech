fbe10a409e2ffeefa011b2e4fe9f76af9d28adf9
Song Li <songl@outlook.com>
2020-04-13 21:14:14 +0800

    Select locale per service provider

--- app.R
+++ app.R
@@ -9,26 +9,54 @@ ui <- fluidPage(
         sidebarPanel(
             selectInput("service",
                         "Pick a service provider:",
-                        c("Amazon" = "amazon",
-                          "Google" = "google",
-                          "Microsoft" = "microsoft"),
-                        selected = "microsoft"),
+                        c("Amazon",
+                          "Google",
+                          "Microsoft"),
+                        selected = "Microsoft"),
+            textInput("key",
+                      "Key for the selected service:"),
+            uiOutput("locale_selector"),
             tags$hr(),
             helpText("View", a("source code on GitHub", href="https://github.com/boltomli/shiny-speech", target="_blank"))
         ),
 
         mainPanel(
-            helpText("Key for authentication might be required for specific service provider."),
-            tags$hr(),
-            tableOutput("voices")
+            tableOutput("voices"),
         )
     )
 )
 
 server <- function(input, output) {
 
+    service_provider <- reactive({
+        switch (input$service,
+            "Amazon" = "amazon",
+            "Google" = "google",
+            "Microsoft" = "microsoft"
+        )
+    })
+    output$locale_selector <- renderUI({
+        req(service_provider())
+        service_provider = service_provider()
+        if (input$key == "") {
+            if (service_provider == "microsoft") {
+                selectInput("locale",
+                            "Locale:",
+                            unique(tts_voices(service = service_provider)["language_code"]))
+            } else {
+                helpText("Follow document of each service provider for authentication method.")
+            }
+        } else {
+            if (tts_auth(service = service_provider, key_or_json_file = input$key)) {
+                selectInput("locale",
+                            "Locale:",
+                            unique(tts_voices(service = service_provider)["language_code"]))
+            }
+        }
+    })
     output$voices <- renderTable({
-        tts_voices(service = input$service)
+        req(service_provider())
+        tts_voices(service = service_provider())
     })
 }
 
